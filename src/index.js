2084b94434e1755376801dad23bf962f1f4fbb4c
Stephen J. Butler <stephen.butler@gmail.com>
2019-07-09 22:48:01 -0500

    Skeleton app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const bunyan = require('bunyan');
+
+const log = bunyan.createLogger({ name: 'cloudwatchEventFormat' });
+
+
+/**
+ * Entry point for the lambda function.
+ */
+exports.handler = async (event, context) => {
+};
