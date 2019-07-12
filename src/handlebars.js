f883491a882555cf96d71dcdb16ca5dc9d81bc2e
Stephen J. Butler <stephen.butler@gmail.com>
2019-07-12 00:29:30 -0500

    Add some code comments

--- src/handlebars.js
+++ src/handlebars.js
@@ -34,6 +34,48 @@ const fs_readFile = util.promisify(fs.readFile),
       fs_stat = util.promisify(fs.stat);
 
 
+/**
+ * Read a template file and compile it with Handlebars. This turns off HTML
+ * escaping since all SNS Publish Message formats are plain text. The template
+ * file is read with UTF-8 encoding.
+ *
+ * The compilation results are cached the first time the template is loaded.
+ *
+ * @param {string} file Full path to the file to load.
+ * @return {function} Compiled Handlebars template.
+ */
+async function compileTemplateFile(file) {
+    if (!_compileTemplateFileCache[file]) {
+        const template = await fs_readFile(file, 'utf8');
+        _compileTemplateFileCache[file] = handlebars.compile(
+            template,
+            { noEscape: true }
+        );
+    }
+
+    return _compileTemplateFileCache[file];
+}
+const _compileTemplateFileCache = {};
+
+/**
+ * Get the list of possible template files, based on the event source and
+ * detail type. It will use these files, in this order, if they exist:
+ *
+ * - source/detail-type.format.hbs
+ * - source/default.format.hbs
+ * - default.format.hbs
+ *
+ * The formats are all the ones defined for SNS Publish Message, with the
+ * special format `_subject_` for the Subject parameter.
+ *
+ * @param {string} eventSource The event `source` value.
+ * @param {string} eventDetailType The event `detail-type` value.
+ * @param {string} templateDir Optional template directory to use. Returned files
+ *      will be relative to this value.
+ * @return {object} Object where each property is the format and the value is
+ *      the file to use. If a file is not found for a format then it will not
+ *      have a property.
+ */
 async function getTemplateFiles(eventSource, eventDetailType, templateDir = TEMPLATEDIR) {
     const hasEventSource = !!eventSource && !INVALIDPATHPART_RE.test(eventSource);
     if (!hasEventSource)
@@ -67,19 +109,15 @@ async function getTemplateFiles(eventSource, eventDetailType, templateDir = TEMP
     return result;
 }
 
-const _compileTemplateFileCache = {};
-async function compileTemplateFile(file) {
-    if (!_compileTemplateFileCache[file]) {
-        const template = await fs_readFile(file, 'utf8');
-        _compileTemplateFileCache[file] = handlebars.compile(
-            template,
-            { noEscape: true }
-        );
-    }
-
-    return _compileTemplateFileCache[file];
-}
-
+/**
+ * Render an event using the best template possible using Handlebars.
+ *
+ * @param {object} event CloudWatch Event will a `source` and `detail-type`.
+ * @param {string} templateDir Optional template directory to use. Returned files
+ *      will be relative to this value.
+ * @return {object} Returns a `message` and `subject` with the rendered values,
+ *      ready to be passed to SNS Publish.
+ */
 async function render(event, templateDir = TEMPLATEDIR) {
     const files = await getTemplateFiles(event.source, event['detail-type'], templateDir);
     const message = {
