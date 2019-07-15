41271273e5f5eba277b5affd5083c57bf6f3880c
Stephen J. Butler <stephen.butler@gmail.com>
2019-07-15 16:59:23 -0500

    Add gulp build system

---
+++ Gulpfile.js
@@ -0,0 +1,46 @@
+const { series, parallel, src, dest } = require('gulp');
+const _clean = require('gulp-clean');
+const _zip = require('gulp-zip');
+const { exec } = require('child_process');
+
+function clean() {
+    return src(['build/', 'dist/'], { read: false })
+        .pipe(_clean());
+}
+
+function hbs() {
+    return src('src/**/*.hbs')
+        .pipe(dest('build/src/'));
+}
+
+function js() {
+    return src('src/**/*.js')
+        .pipe(dest('build/src/'));
+}
+
+function npm() {
+    return exec(
+        'npm install',
+        {
+            cwd: 'build/',
+            env: {
+                ...process.env,
+                NODE_ENV: process.env.NODE_ENV || 'production',
+            }
+        }
+    )
+}
+
+function pkg() {
+    return src('package*.json')
+        .pipe(dest('build/'));
+}
+
+function zip() {
+    return src('build/*')
+        .pipe(_zip('cloudwatchEventFormat.zip'))
+        .pipe(dest('dist/'));
+}
+
+exports.clean = clean
+exports.default = series(parallel(hbs, js, series(pkg, npm)), zip);
