c825067e1eb1c262383df45028f3273be54ded89
Stephen J. Butler <stephen.butler@gmail.com>
2019-07-15 16:07:35 -0500

    Add basic index.js implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+const AWS = require('aws-sdk');
 const bunyan = require('bunyan');
 
+const hbs = require('handlebars');
+
 const log = bunyan.createLogger({ name: 'cloudwatchEventFormat' });
+const awsSNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-
-
+const TEMPLATE_DIR = process.env.TEMPLATE_DIR;
+const SNS_TOPIC_ARN = process.env.SNS_TOPIC_ARN;
 
 /**
  * Entry point for the lambda function.
  */
 exports.handler = async (event, context) => {
+    const { message, subject } = await hbs.render(event, TEMPLATE_DIR);
+
+    log.info('Publishing "%s" to SNS Topic: %s', subject, SNS_TOPIC_ARN);
+    await awsSNS.publish({
+        Message: JSON.stringify(message),
+        MessageStructure: 'json',
+        Subject: subject,
+        TopicArn: SNS_TOPIC_ARN,
+    });
 };
